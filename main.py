656f54be1fc9bd78a8c568af9f79405a485d37c5
EC <enda@endacrossan.com>
2024-07-16 14:23:32 +0100

    Added add_to() function to read session state of text_input area

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    print(todo)
+
 todos = functions.get_todos()
 
 st.title("Enda's ToDo App")
@@ -10,5 +14,7 @@ st.write("This should help increase productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo item:", placeholder="Add new to do item...")
+st.text_input(label="Enter a todo item:", placeholder="Add new to do item...", on_change=add_todo, key="new_todo")
+
+# st.session_state # used to output session state to UI
 
